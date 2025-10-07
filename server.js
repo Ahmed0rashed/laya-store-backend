27665973c40631c2b49fbcd4a1cb33b4cdad0013
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-10-07 17:57:14 +0300

    solve the problem in deploy

--- server.js
+++ server.js
@@ -37,3 +37,4 @@ process.on('SIGTERM', () => {
     console.log(' Process terminated!');
   });
 });
+
--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // Check if MONGODB_URI is defined
+    if (!process.env.MONGODB_URI) {
+      console.error('MONGODB_URI environment variable is not defined');
+      process.exit(1);
+    }
+
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
